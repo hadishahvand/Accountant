1cb0a6af4295ffc8671dc818217402ab737d7d28
hadi shahvand <hadi.shahvand@gmail.com>
2020-12-04 16:31:14 +0330

    expense model is there

--- web/models.py
+++ web/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User)
-    
+    user = models.ForeignKey(User, on_delete=())
