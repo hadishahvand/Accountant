596244acaaa038d1891265d6a18dadc69538db0e
hadi shahvand <hadi.shahvand@gmail.com>
2020-12-08 22:20:36 +0330

    Add submit expense and income

--- web/views.py
+++ web/views.py
@@ -1,3 +1,32 @@
+from json import JSONEncoder
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from web.models import Expense, Income, User, Token
+from datetime import datetime
 
-# Create your views here.
+
+@csrf_exempt
+def submit_expense(request):
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token = this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+    Expense.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'], date=date)
+    
+    return JsonResponse({
+        'status': 'ok',
+    })
+
+
+@csrf_exempt
+def submit_income(request):
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token = this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+    Income.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'], date=date)
+    
+    return JsonResponse({
+        'status': 'ok',
+    })
